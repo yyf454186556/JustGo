4852271806d457ab6c448e561cbbd8938a715676
Dogtwo <454186556@QQ.com>
2020-06-20 10:26:59 +0800

    函数，可变长参数，defer方法

--- Session10/func/func_test.go
+++ Session10/func/func_test.go
@@ -39,3 +39,29 @@ func TestFn(t *testing.T) {
 	tsSF := timeSpent(slowFun)
 	t.Log(tsSF(10))
 }
+
+func Sum (ops ... int) int {
+	ret := 0
+	for _, s := range ops {
+		ret += s
+	}
+	return ret
+}
+
+func TestVarParam(t *testing.T) {
+	t.Log(Sum(1,2,3,4))
+	t.Log(Sum(5,6,7,8,9))
+}
+
+func Clear() {
+	fmt.Println("Clear resources in Clear function.")
+}
+
+func TestDefer(t *testing.T){
+	defer func() { //这个内联函数直到TestDefer执行完毕，返回前才会执行。 通常用于清理资源，释放锁等功能
+		defer Clear()
+		t.Log("Clear resources")
+	}()
+	t.Log("Started")
+	panic("Fatal error") //defer 仍会执行 //panic是Go中程序异常中断，抛出致命错误
+}
