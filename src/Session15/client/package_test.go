7c0aaf78e0a9710796034478561ac64668563462
Dogtwo <454186556@QQ.com>
2020-06-27 11:10:24 +0800

    包和依赖管理

--- src/Session15/client/package_test.go
+++ src/Session15/client/package_test.go
@@ -1,13 +1,11 @@
 package client
 
 import (
-	"fmt"
 	"testing"
 	"Session15/series"
 )
 
-//import "Session15/series"
-
 func TestPackage(t *testing.T){
-	fmt.Println(series.GetFibonacciSerie(5))
+	t.Log(series.GetFibonacciSerie(5))
+	//t.Log(series.square(5)) //包中小写的方法是不能被访问的，会提示错误cannot refer to unexported name series.squareg
 }
--- src/Session15/series/my_series.go
+++ src/Session15/series/my_series.go
@@ -1,9 +1,31 @@
 package series
 
+import (
+	"fmt"
+)
+
 func GetFibonacciSerie(n int) []int {
 	ret := []int{1,1}
 	for i := 2; i < n; i++ {
 		ret = append(ret, ret[i-2] + ret[i-1])
 	}
 	return ret
+}
+
+//首字母小写在包外不可以访问
+func square(n int) int {
+	return n * n
+}
+
+//这里是在同一个源码文件里的两个同名的init方法，并不会报错，而是会依次执行
+func init(){
+	fmt.Println("init3")
+}
+
+func init(){
+	fmt.Println("init1")
+}
+
+func init(){
+	fmt.Println("init2")
 }
